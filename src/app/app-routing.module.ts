01dba8f0b950ac51be4cf5c87c9495dcb8ad9291
Jolver Andre <jquevedo@elmodelador.com.pe>
2018-12-07 19:23:49 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { ArticulosComponent } from './pages/articulos/articulos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'articulos', component: ArticulosComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
